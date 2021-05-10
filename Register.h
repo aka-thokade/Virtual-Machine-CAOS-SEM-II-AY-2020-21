cf28733a2edf8169e0f751602eaf245dfbc70128
Sejalsugu26 <sejal.babar19@vit.edu>
2021-05-10 19:44:37 +0530

    IC changed from array to int

--- Register.h
+++ Register.h
@@ -7,7 +7,7 @@
 class Register {
 	public:
 		char IR[4], R[4];
-		int M[2], IC[2], C;
+		int M[2], IC, C;
 		char buffer[40];
 		std::vector<std::string> instruction;
 		std::vector<std::string> ins_word;
